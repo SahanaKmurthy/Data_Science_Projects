06728e873d85573d59663b0864a94ba496501e0a
Sahana Krishna Murthy <kmurthy.sahana@gmail.com>
2025-05-29 20:07:54 -0400

    Update app.py

--- Deep Learning/ANN/app.py
+++ Deep Learning/ANN/app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pickle 
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('Deep Learning/ANN/model.h5')
 
 # Load encoders and scaler
 with open ('label_encoder_gender.pkl' , 'rb') as file:
